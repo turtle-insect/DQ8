16362684fdd24e2fd52079331e862b1c9569a885
turtle-insect <turtle.insect@gmail.com>
2026-04-26 07:32:30 +0900

    Visual Studio 2026 & .NET 10

--- DQ8/MainWindow.xaml.cs
+++ DQ8/MainWindow.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.Win32;
-using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace DQ8
 {
 	/// <summary>
-	/// MainWindow.xaml の相互作用ロジック
+	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
@@ -14,7 +21,6 @@ namespace DQ8
 
 		public MainWindow()
 		{
-			Info.Instance();
 			InitializeComponent();
 		}
 
@@ -56,11 +62,6 @@ namespace DQ8
 		private void MenuItemExit_Click(object sender, RoutedEventArgs e)
 		{
 			Close();
-		}
-
-		private void MenuItemAbout_Click(object sender, RoutedEventArgs e)
-		{
-			new AboutWindow().ShowDialog();
 		}
 
 		private void MenuItemRecipeCheck_Click(object sender, RoutedEventArgs e)
